fcc368638d08183692443605a37fc3c4719608b3
randalmaile <randalmaile@gmail.com>
2016-08-13 14:11:52 -0700

    Revert "Revert "asdf""
    This reverts commit 55393feb77cd41fee18e68e1ed8efc9f6d199e26.

---
+++ src/app/filterx/filterx.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-filterx',
+  templateUrl: 'filterx.component.html',
+  styleUrls: ['filterx.component.css']
+})
+export class FilterxComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
